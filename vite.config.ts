bd202f0de7d17ad4b3fffd894f4ba8429abfc323
Harshit <Developer2@sundaramtech.local>
2026-01-22 16:47:11 +0530

    Update portfolio code and fixes for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 import { componentTagger } from "lovable-tagger";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/harshit-s-dev-portfolio/",
+
   server: {
     host: "::",
     port: 8080,
@@ -12,7 +14,9 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
